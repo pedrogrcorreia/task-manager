5de1159617d87431169d65b896fb2dd8d27ab416
Pedro Correia <pedrogrcorreia@gmail.com>
2023-03-16 19:32:34 +0000

    creating routes for getting users and tasks

--- src/index.js
+++ src/index.js
@@ -12,9 +12,28 @@ app.post('/users', (req, res) => {
     const user = new User(req.body)
 
     user.save().then(() =>{
-        res.send(user)
+        res.status(201).send(user)
     }). catch((error) => {
         res.status(400).send(error)
+    })
+})
+
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((error) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    User.findById(req.params.id).then((user) => {
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((error) => {
+        res.status(500).send()
     })
 })
 
@@ -22,9 +41,28 @@ app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
 
     task.save().then(() => {
-        res.send(task)
+        res.status(201).send(task)
     }).catch((error) => {
         res.status(400).send(error)
+    })
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((error) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    Task.findById(req.params.id).then((task) => {
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch((error) => {
+        res.status(500).send()
     })
 })
 
