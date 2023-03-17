08020d588170fbba5ffd81245e23020b897c8201
Pedro Correia <pedrogrcorreia@gmail.com>
2023-03-17 16:07:43 +0000

    login and logout sessions

--- src/index.js
+++ src/index.js
@@ -15,16 +15,3 @@ app.use(taskRouter)
 app.listen(port, () =>{
     console.log('Server is running on port ', port)
 })
-
-const jwt = require('jsonwebtoken')
-
-const myFunction = async () => {
-    const token = jwt.sign({_id: 'abc123'}, 'thisismynewcourse', {expiresIn: '7 days'})
-
-    console.log('token:', token)
-
-    const data = jwt.verify(token, 'thisismynewcourse')
-    console.log(data)
-}
-
-myFunction()
