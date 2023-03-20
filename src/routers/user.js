e8e15464fdd036b5cf2555d135c4ad20cddf7df5
Pedro Correia <pedrogrcorreia@gmail.com>
2023-03-20 16:33:14 +0000

    cropping images and setting format as png for all uploaded images using sharp

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const multer = require('multer')
+const sharp = require('sharp')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
@@ -108,7 +109,12 @@ router.delete('/users/me', auth, async (req, res) => {
 })
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-    req.user.avatar = req.file.buffer 
+    const buffer = await sharp(req.file.buffer)
+        .resize({width: 200, height: 200})
+        .png()
+        .toBuffer()
+    
+    req.user.avatar = buffer 
 
     
     await req.user.save()
@@ -137,7 +143,7 @@ router.get('/users/:id/avatar', async (req, res) => {
             throw new Error()
         }
 
-        res.set('Content-Type', 'image/jpg')
+        res.set('Content-Type', 'image/png')
         res.send(user.avatar)
     } catch(e){
         res.status(404).send()
